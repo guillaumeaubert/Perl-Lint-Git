1561c223f2eb1fe64afb773c658d21b00daafbd7
Guillaume Aubert <aubertg@cpan.org>
2017-03-19 14:51:37 -0700

    Update copyright year.

--- lib/Perl/Lint/Git.pm
+++ lib/Perl/Lint/Git.pm
@@ -434,7 +434,7 @@ C<< <aubertg at cpan.org> >>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2015-2016 Guillaume Aubert.
+Copyright 2015-2017 Guillaume Aubert.
 
 This code is free software; you can redistribute it and/or modify it under the
 same terms as Perl 5 itself.
